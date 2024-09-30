21ecb361ff4292ec7ab653431094247c29623ddb
h658 <raiswanim@gmail.com>
2024-09-30 16:43:41 +0545

    Initial commit for Digital Clock project

---
+++ theme.js
@@ -0,0 +1,15 @@
+// const button = document.querySelector('button');
+// const changeIcon = document.getElementById('changeIcon');
+// const body = document.body;
+
+// const changeMode=()=>{
+//     if(body.classList.contains('darkMode')){
+//         body.classList.replace('darkMode', 'lightMode')
+//         changeIcon.classList.replace('bi-moon-stars-fill', 'bi-brightness-high-fill')
+//     }else{
+//         body.classList.replace('lightMode', 'darkMode')
+//         changeIcon.classList.replace('bi-brightness-high-fill', 'bi-moon-stars-fill')    }
+// }
+
+// button.addEventListener('click', changeMode)
+
